82eba4433df2609c9a07a98ac7e4ee9a63e5bfa2
Hans <hans@pets.com>
2020-09-06 16:53:10 +0300

    fuck landing pages

--- src/js/images-scrolling.js
+++ src/js/images-scrolling.js
@@ -4,19 +4,17 @@ export function enableImagesScrolling() {
     window.addEventListener('scroll', ev => {
         try {
             const scrollPos = window.scrollY;
-            console.log("Height" +  scrollPos / window.innerHeight * 100);
-            let lastBottom = 0;
+            console.log("h:" + scrollPos);
             for(const id of ids) {
                 const element = document.getElementById(id);
                 const startScroll = parseInt(element.getAttribute("startScroll")) * window.innerHeight / 100;
-                let top = 0;
-                if(scrollPos >= startScroll) {
-                    top = startScroll;
-                    lastBottom = Math.max(element.offsetTop + element.scrollHeight, scrollPos);
+                if(scrollPos > startScroll) {
+                    element.style.position = 'absolute';
+                    element.style.top = Math.round(startScroll).toString() + "px";
                 } else {
-                    top = lastBottom;
+                    element.style.position = 'fixed';
+                    element.style.top = null;
                 }
-                element.style.top = Math.round(top).toString() + "px";
             }
         } catch (e) {
             console.error(JSON.stringify(e))
--- src/js/smooth-scrolling.js
+++ src/js/smooth-scrolling.js
@@ -13,7 +13,8 @@ export function enableSmoothScrolling(){
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top,
+                easing: 'linear'
             }, 800, function(){
 
                 // Add hash (#) to URL when done scrolling (default click behavior)

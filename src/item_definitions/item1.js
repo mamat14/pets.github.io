bafdf3977635d5ecad1a2f6a81687c97362dbb19
Hans <hans@pets.com>
2020-09-06 21:10:33 +0300

    qq

--- src/item_definitions/item1.js
+++ src/item_definitions/item1.js
@@ -1,4 +1,11 @@
 export const item = {
   id: "Some item id",
   name: "Some item name",
+  description: "",
+  images: [
+    {type: "qwer",
+     url: ""},
+    {type: "preview",
+     url: ""}
+  ]
 };

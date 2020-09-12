3a806f798070494d165821b847d1b255bbbcc0d3
Hans <hans@pets.com>
2020-09-12 22:02:41 +0300

    Initial dist subtree commit

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap';
-import {enableSmoothScrolling} from "./smooth-scrolling";
-import {enableImagesScrolling} from "./images-scrolling";
-
-$(document).ready(function () {
-    console.log('Webpack loaded!');
-    enableSmoothScrolling();
-    enableImagesScrolling();
-});
